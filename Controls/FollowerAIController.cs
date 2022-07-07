91fb1260f3f3586eef2682720dba15a1aa68fb2d
Snowmau <glenramosesteban@gmail.com>
2022-07-07 04:00:15 -1000

    Updated Following Behavior and Target States

--- Controls/FollowerAIController.cs
+++ Controls/FollowerAIController.cs
@@ -8,58 +8,56 @@ using System;
 
 namespace coldheart_controls {
     public enum TargetState {
-        MainCharacter,
-        CurrentCharacter,
-        AssignedTargetCharacter,
-        ThisCharacter
+        FollowTargetCharacter,
+        FollowCurrentCharacter,
+        FollowNoOne
     }
     public class FollowerAIController : MonoBehaviour {
         bool isControlledByPlayer;
         bool isAIControlStateUpdated;
         CharacterManager characterManager;
+        GameObject currentCharacter;
         Movement movement;
         NavMeshAgent navMeshAgent;
         public TargetState targetState;
-        public Transform followTarget;
-        public Transform assignedTargetCharacter;
-        public void SetTargetState(TargetState targetState) {
-            this.targetState = targetState;
-        }
+        public Transform currentFollowTarget;
+        public Transform assignedFollowTarget;
         void Awake() {
             characterManager = FindObjectOfType<CharacterManager>();
             movement = GetComponent<Movement>();
             navMeshAgent = GetComponent<NavMeshAgent>();
         }
-        void OnEnable() {
-            SetTargetState(TargetState.MainCharacter);
-            
+        void OnEnable() {  
+            targetState = TargetState.FollowTargetCharacter;
+
             characterManager.onSwitchCharacterAction += UpdateAIControlStatus;
             characterManager.onSwitchCharacterAction += UpdateTargetState;
-            characterManager.onAllPlayerCharactersFollowTargetPlayer += FollowAssignedTargetCharacter;
+            characterManager.onAllPlayerCharactersFollowTargetPlayer += FollowTargetCharacter;
         }
         void OnDisable() {
             if (characterManager.CheckIfCharacterIsAPlayerCharacter(gameObject)) {
                 characterManager.onSwitchCharacterAction -= UpdateAIControlStatus;
-                characterManager.onAllPlayerCharactersFollowTargetPlayer -= FollowCurrentCharacter;
+                characterManager.onSwitchCharacterAction -= UpdateTargetState;
+                characterManager.onAllPlayerCharactersFollowTargetPlayer -= FollowTargetCharacter;
             }
         }
         void Update() {
-            // Although having an AI control status creates a state, the state is very
-            // simple and only needs it once rather than having it checked every frame.
             if (!isAIControlStateUpdated) {
                 UpdateAIControlStatus();
-                FollowMainCharacter();
-                assignedTargetCharacter = characterManager.GetMainPlayerCharacter().transform;
             }
 
             if (isControlledByPlayer) {return;}
 
-            if (targetState == TargetState.AssignedTargetCharacter) {
-                movement.FollowTarget(assignedTargetCharacter);
-
+            if (targetState == TargetState.FollowTargetCharacter) {
+                if (assignedFollowTarget == null) {
+                    assignedFollowTarget = characterManager.GetMainPlayerCharacter().transform;
+                }
+                else {
+                    movement.FollowTarget(assignedFollowTarget);
+                }
             }
-            else {
-                movement.FollowTarget(followTarget);
+            else if (targetState == TargetState.FollowCurrentCharacter) {
+                movement.FollowTarget(currentFollowTarget);
             }
         }
         void UpdateAIControlStatus() {
@@ -73,66 +71,51 @@ namespace coldheart_controls {
 
             // For some reason GetCurrentPlayerCharacter() returns null at Start and on the 
             // ... first Update(), so here's a quick fix. 
-            if (characterManager.GetCurrentPlayerCharacter() != null) {
+            GameObject currentCharacter = characterManager.GetCurrentPlayerCharacter();
+            if (currentCharacter != null) {
                 isAIControlStateUpdated = true;
             }
         }
-        void FollowMainCharacter() {
-            targetState = TargetState.MainCharacter;
-            GameObject mainCharacter = characterManager.GetMainPlayerCharacter();
-            followTarget = mainCharacter.transform;
+        void FollowTargetCharacter() {
+            targetState = TargetState.FollowTargetCharacter;
+            GameObject currentCharacter = characterManager.GetCurrentPlayerCharacter();
+            assignedFollowTarget = characterManager.GetCurrentPlayerCharacter().transform;
         }
         void FollowCurrentCharacter() {
-            targetState = TargetState.CurrentCharacter;
+            targetState = TargetState.FollowCurrentCharacter;
             GameObject currentCharacter = characterManager.GetCurrentPlayerCharacter();
-            followTarget = currentCharacter.transform;
-        }
-        void FollowAssignedTargetCharacter() {
-            GameObject currentPlayerCharacter = characterManager.GetCurrentPlayerCharacter();
-            if (this.gameObject == currentPlayerCharacter) {return;}
-
-            targetState = TargetState.AssignedTargetCharacter;
-            assignedTargetCharacter = currentPlayerCharacter.transform; // does not change until FollowMe() is called again
+            currentFollowTarget = currentCharacter.transform;
         }
         public void SwitchTargetState() {
-            if (targetState == TargetState.MainCharacter) {
-                SetTargetState(TargetState.CurrentCharacter);
+            if (targetState == TargetState.FollowTargetCharacter) {
                 FollowCurrentCharacter();
                 print(gameObject.name + " is following current character!");
             }
-            else if (targetState == TargetState.CurrentCharacter) {
-                SetTargetState(TargetState.ThisCharacter);
-                followTarget = this.gameObject.transform;
+            else if (targetState == TargetState.FollowCurrentCharacter) {
+                targetState = TargetState.FollowNoOne;
                 print(gameObject.name + " is following no one!");
             }            
-            else if (targetState == TargetState.ThisCharacter) {
-                SetTargetState(TargetState.MainCharacter);
-                FollowMainCharacter();
-                print(gameObject.name + " is following main character!");
+            else if (targetState == TargetState.FollowNoOne) {
+                targetState = TargetState.FollowTargetCharacter;
+                print(gameObject.name + " is following its target!");
             }
-            else if (targetState == TargetState.AssignedTargetCharacter) {
-                SetTargetState(TargetState.MainCharacter);
-                FollowMainCharacter();
-                print(gameObject.name + " is following main character!");
-            } 
         }
         public void UpdateTargetState() {
-            if (targetState == TargetState.MainCharacter) {
-                FollowMainCharacter();
-            }
-            else if (targetState == TargetState.CurrentCharacter) {
+            if (targetState == TargetState.FollowCurrentCharacter) {
                 FollowCurrentCharacter();
             }
         }
-        // Visualize follow target connections
         void OnDrawGizmos() {
-            if (followTarget == null) return;
-
-            Gizmos.color = Color.blue;
-            Gizmos.DrawLine(transform.position, followTarget.position);
-
-            Gizmos.color = Color.red;
-            Gizmos.DrawLine(transform.position, assignedTargetCharacter.position);
+            if (assignedFollowTarget == null) return;
+            
+            if (targetState == TargetState.FollowTargetCharacter) {
+                Gizmos.color = Color.blue;
+                Gizmos.DrawLine(transform.position, assignedFollowTarget.position);
+            }
+            else if (targetState == TargetState.FollowCurrentCharacter) {
+                Gizmos.color = Color.red;
+                Gizmos.DrawLine(transform.position, currentFollowTarget.position);
+            }
         }
     }
 }
